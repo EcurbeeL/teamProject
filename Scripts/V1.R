80bfcc0e819afb1e1fbc6f0e971415d217bf4068
Ivan <ivan.neufert@googlemail.com>
2019-11-17 19:13:47 +0100

    change V1

--- Scripts/V1.R
+++ Scripts/V1.R
@@ -8,7 +8,7 @@ train <- read.csv("C:/Users/Ivan/Documents/teamProject/HouseData/train.csv")
 test <- read.csv("C:/Users/Ivan/Documents/teamProject/HouseData/test.csv")
 
 # fit our model
-model <- randomForest(SalePrice ~ LotArea + OverallQual + YearBuilt + TotRmsAbvGrd, data = train, mtry=20, ntree=2000, importance=TRUE )
+model <- randomForest(SalePrice ~ LotArea + OverallQual + YearBuilt + TotRmsAbvGrd, data = train)
 
 # use our model to make predictions
 predicted_prices <- predict(model, newdata = test)
@@ -20,4 +20,6 @@ head(predicted_prices)
 my_submission <- data_frame('Id' = as.integer(test$Id), 'SalePrice' = predicted_prices)
 # save our file
 write_csv(my_submission, 'Submissions/V1.csv')
+
+summary(train)
 
