a2bd4f0c3bf08ce766176339841eebb8a08f98cd
Imspiess <57508841+Imspiess@users.noreply.github.com>
2019-11-26 17:43:00 +0100

    V

--- Immanuel/Kids_RandomF.R
+++ Immanuel/Kids_RandomF.R
@@ -1,17 +1,20 @@
-
 library(tidyverse)
+library(data.table)
+library(caret)
+library(ranger)
 
+#Read Files
+ 
 train <- data.table::fread('/Users/immanuelspiess/Documents/HTWG /8/Teamprojekt/GIt/PBS-Kids/data-science-bowl-2019/s_train.csv', stringsAsFactors = F)
 train_labels <- data.table::fread('/Users/immanuelspiess/Documents/HTWG /8/Teamprojekt/GIt/PBS-Kids/data-science-bowl-2019/train_labels.csv', stringsAsFactors = F)
+test <- data.table::fread('/Users/immanuelspiess/Documents/HTWG /8/Teamprojekt/GIt/PBS-Kids/data-science-bowl-2019/test.csv')
 
 
-#Daten reduzieren
 train %>%
   filter(type =="Assessment") %>% 
   distinct(installation_id) %>% 
   left_join(train, by = "installation_id") -> train
 
-test <- data.table::fread('/Users/immanuelspiess/Documents/HTWG /8/Teamprojekt/GIt/PBS-Kids/data-science-bowl-2019/test.csv')
 
 test %>% 
   filter(type == "Assessment") %>% 
@@ -19,13 +22,14 @@ test %>%
   distinct(installation_id, .keep_all = T) %>% 
   select(installation_id, title) -> last_assessment
 
-train_labels %>% 
-  group_by(title) %>% 
-  summarise(accuracy_group = median(accuracy_group, na.rm = T)) %>% 
-  ungroup() -> median_table
 
-last_assessment %>% 
-  left_join(median_table, by = "title") %>% 
-  select(-title) -> submission
+train(as.factor(accuracy_group) ~ title, 
+      data = train_labels,
+      method = "ranger") -> rf_fit
+
+
+pred <- predict(rf_fit, last_assessment)
+last_assessment$accuracy_group <- pred
+last_assessment %>% select(-title) -> submission
 
 write.csv(submission, "submission.csv", row.names = F)
